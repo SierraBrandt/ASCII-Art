81d0da40cc2b234c8648504ff9d9afd7a0f3a358
Sierra Brandt <44011600+SierraBrandt@users.noreply.github.com>
2021-09-07 19:22:23 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//Creates a potrait of a lion made out of characters
+//Author: Sierra Brandt
+#include <iostream>
+
+int main() {
+  std::cout << "              ,  ,, ,"<< std::endl;
+  std::cout << "       , ,; ; ;;  ; ;  ;"<< std::endl;
+  std::cout << "    , ; ';  ;  ;; .-''\\ ; ;"<< std::endl;
+  std::cout << " , ;  ;`  ; ,; . / /8b \\ ; ;"<< std::endl;
+  std::cout << " `; ; .;'         ;,\\8 |  ;  ;"<< std::endl;
+  std::cout << " ` ;/   / `_       ; ;;    ;  ; ;"<< std::endl;
+  std::cout << "     |/.'  /9)     ;  ; `    ;  ; ;"<< std::endl;
+  std::cout << "    ,/'           ; ; ;  ;   ; ; ; ;"<< std::endl;
+  std::cout << "   /_             ;    ;  `    ;  ;"<< std::endl;
+  std::cout << "  `?8P''  .      ;  ; ; ; ;     ;  ;;"<< std::endl;
+  std::cout << "  | ;  .:: `      ;; ; ;   `  ;  ;"<< std::endl;
+  std::cout << "  `' `--._      ;;  ;;  ; ;   ;   ;"<< std::endl;
+  std::cout << "   `-..__..--''   ; ;    ;;   ; ;   ;"<< std::endl;
+  std::cout << "               ;    ; ; ;   ;     ; "<< std::endl;
+
+}
